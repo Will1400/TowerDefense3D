d011905a03c40c15a5f267b3734053985d193944
Will1400 <will1400@edu.campusvejle.dk>
2019-10-28 14:36:07 +0100

    Add project files.

---
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int[,] Map;
+    public Transform[,] objMap;
+    public int Seed = 10;
+
+    [SerializeField]
+    private Transform tilePrefab;
+    [SerializeField]
+    private GameObject startPointPrefab;
+    [SerializeField]
+    private GameObject endPointPrefab;
+
+    [SerializeField]
+    private Material pathMaterial;
+    [SerializeField]
+    private Vector2Int mapSize;
+    [SerializeField, Range(0, 1)]
+    private float outlinePercent;
+
+    private Transform mapHolder;
+
+    private AStar aStar;
+    private Vector2Int startPoint;
+    private Vector2Int endPoint;
+
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    public void GenerateMap()
+    {
+        Setup();
+
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                Map[x, y] = 0;
+                Vector3 tilePosition = new Vector3(-mapSize.x / 2 + .5f + x, 0, -mapSize.y / 2 + .5f + y);
+                Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.identity).transform;
+                newTile.localScale = new Vector3(1 * (1 - outlinePercent), newTile.localScale.y, 1 * (1 - outlinePercent));
+                newTile.localPosition += new Vector3(0, .5f - newTile.localScale.y / 2);
+                newTile.SetParent(mapHolder.Find("Tiles"));
+                objMap[x, y] = newTile;
+            }
+        }
+        endPoint = GenerateEndPoint();
+        startPoint = GenerateStartPoint();
+        List<Vector2Int> paths = aStar.GetPath(Map, startPoint, endPoint);
+
+        foreach (var path in paths)
+        {
+            objMap[path.x, path.y].GetComponent<Renderer>().material = pathMaterial;
+        }
+    }
+
+    void Setup()
+    {
+        aStar = new AStar();
+        Random.InitState(Seed + (int)mapSize.x + (int)mapSize.y);
+
+        if (mapSize.x < 4)
+            mapSize.x = 4;
+        if (mapSize.y < 4)
+            mapSize.y = 4;
+
+        Map = new int[mapSize.x, mapSize.y];
+        objMap = new Transform[mapSize.x, mapSize.y];
+
+        string holderName = "Generated Map";
+        if (transform.Find(holderName))
+            DestroyImmediate(transform.Find(holderName).gameObject);
+        mapHolder = new GameObject(holderName).transform;
+        mapHolder.parent = transform;
+
+        Transform tiles = new GameObject("Tiles").transform;
+        tiles.parent = mapHolder;
+
+        Transform objects = new GameObject("Objects").transform;
+        objects.parent = mapHolder;
+
+        Transform path = new GameObject("Path").transform;
+        path.parent = mapHolder;
+    }
+
+    Vector2Int GenerateEndPoint()
+    {
+        int x = Random.Range(2, mapSize.x / 4);
+        int y = Random.Range(2, mapSize.y / 4);
+        Map[x, y] = 10;
+        Instantiate(endPointPrefab, new Vector3(-mapSize.x / 2 + .5f + x, 1, -mapSize.y / 2 + .5f + y), Quaternion.identity, mapHolder.Find("Path"));
+
+        return new Vector2Int(x, y);
+    }
+
+    Vector2Int GenerateStartPoint()
+    {
+        int x = Random.Range(1, mapSize.x - 1);
+        int y = Random.Range(1, mapSize.y - 1);
+        Map[x, y] = 9;
+        Instantiate(startPointPrefab, new Vector3(-mapSize.x / 2 + .5f + x, 1, -mapSize.y / 2 + .5f + y), Quaternion.identity, mapHolder.Find("Path"));
+
+        return new Vector2Int(x, y);
+    }
+}
---
+++ Assets/Scripts/Pathfinding/AStar.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AStar 
+{
+
+    private int[,] map;
+
+    public List<Vector2Int> GetPath(int[,] map, Vector2Int start, Vector2Int end)
+    {
+        this.map = map;
+        List<Vector2Int> path = new List<Vector2Int>();
+
+        Node startNode = new Node(start);
+        Node endNode = new Node(end);
+
+        List<Node> openList = new List<Node>();
+        List<Node> closedList = new List<Node>();
+
+        openList.Add(startNode);
+
+        while (openList.Count > 0) // Loop til end is found
+        {
+            Node currentNode = openList[0];
+            int currentIndex = 0;
+
+            currentNode = openList.OrderBy(x => x.F).FirstOrDefault();
+
+            openList.Remove(currentNode);
+            closedList.Add(currentNode);
+
+
+            if (currentNode == endNode) // Found goal
+            {
+                Node current = currentNode;
+
+                while (current != null)
+                {
+                    path.Add(current.Position);
+                    current = current.parent;
+                }
+                return path;
+            }
+
+            List<Node> children = GetValidAdjacentNodes(currentNode);
+
+
+            foreach (Node child in children)
+            {
+                if (closedList.Contains(child))
+                    continue;
+
+                child.G = currentNode.G + 1;
+                child.H = Convert.ToInt32(Math.Pow(child.Position.x - endNode.Position.x, 2) + Math.Pow(child.Position.y - endNode.Position.y, 2));
+                child.F = child.G + child.H;
+
+                foreach (Node openNode in openList)
+                {
+                    if (child.Equals(openNode) && child.G > openNode.G)
+                        continue;
+                }
+                openList.Add(child);
+            }
+        }
+
+        return new List<Vector2Int>();
+    }
+
+    List<Node> GetValidAdjacentNodes(Node node)
+    {
+        List<Node> nodes = new List<Node>();
+        int x = node.Position.x;
+        int y = node.Position.y;
+
+        nodes.Add(ConvertPositionToNode(new Vector2Int(x, y - 1)));
+        nodes.Add(ConvertPositionToNode(new Vector2Int(x, y + 1)));
+        nodes.Add(ConvertPositionToNode(new Vector2Int(x - 1, y)));
+        nodes.Add(ConvertPositionToNode(new Vector2Int(x + 1, y)));
+
+        for (int i = 0; i < nodes.Count; i++)
+        {
+            if (!IsValidPosition(nodes[i].Position))
+            {
+                nodes.RemoveAt(i);
+                i--;
+            }
+        }
+
+        return nodes;
+
+        bool IsValidPosition(Vector2Int pos)
+        {
+            if (pos.x > map.GetUpperBound(0) || pos.y > map.GetUpperBound(1) || pos.x < 0 || pos.y < 0)
+                return false;
+
+            if (map[pos.x, pos.y] != 2)
+                return true;
+
+            return false;
+        }
+
+
+        Node ConvertPositionToNode(Vector2Int pos)
+        {
+            return new Node(pos);
+        }
+    }
+
+
+    class Node : IEquatable<Node>
+    {
+        public int G;
+        public int H;
+        public int F;
+
+        public Vector2Int Position;
+        public Node parent;
+
+        public Node(Vector2Int position, Node parent = null)
+        {
+            Position = position;
+            this.parent = parent;
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Node node)
+                return Position == node.Position;
+
+            return false;
+        }
+
+        public bool Equals(Node other)
+        {
+            return other != null &&
+                   Position.Equals(other.Position);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -38550939;
+            hashCode = hashCode * -1521134295 + EqualityComparer<Vector2>.Default.GetHashCode(Position);
+            return hashCode;
+        }
+    }
+}
