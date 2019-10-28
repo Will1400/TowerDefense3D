851851dd0f5c3d0d9906247a8e768360cba0cbde
Will1400 <will1400@edu.campusvejle.dk>
2019-10-28 14:42:00 +0100

    Files

--- Assets/Scripts/Map/MapEditor.cs
+++ Assets/Scripts/Map/MapEditor.cs
@@ -8,10 +8,14 @@ public class MapEditor : Editor
 {
     public override void OnInspectorGUI()
     {
-        base.OnInspectorGUI();
-
         MapGenerator map = target as MapGenerator;
 
-        map.GenerateMap();
+
+        if (GUILayout.Button("Generate Map"))
+        {
+            map.GenerateMap();
+
+        }
+
     }
 }
