3415996de700f85c41d2b06842f45f974a5cc140
Williamhk <williamhk626@protonmail.com>
2019-11-01 09:38:49 +0100

    Upgraded to hdrp

--- Assets/Scripts/Map/MapEditor.cs
+++ Assets/Scripts/Map/MapEditor.cs
@@ -15,6 +15,7 @@ public class MapEditor : Editor
         {
             MapGenerator map = target as MapGenerator;
             map.GenerateMap();
+            map.RenderMap();
         }
     }
 }
